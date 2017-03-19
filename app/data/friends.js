dcb9686903a47115d38ded29a5d602b57129fdbd
kanwisher <kanwisher@gmail.com>
2017-03-19 14:32:28 -0400

    Heroku Push?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,63 @@
 const friends = 
 [{
 "name": "Patty",
-"photo": "",
-"scores": [3,4,5,4,5,1,3,2,2,5]
+"photo": "/images/patty.png",
+"scores": [3,4,5,4,5,1,3,2,2,5],
+"bio" : "She loves Jesus and would like to share that love with you. If you split a banana milkshake with Patty, it could lead to marriage!"
 },
 {
 "name": "Rick",
-"photo": "",
-"scores": [4,2,3,2,2,2,3,3,2,3]
+"photo": "/images/rick.jpg",
+"scores": [4,2,3,2,2,2,3,3,2,3],
+"bio": "Rick is very confident! He really wants your attention, but doesn't need it (obviously). He's a great match if you don't mind that everything will be about him."
 },
 {
 "name": "Antonia",
-"photo": "",
-"scores": [2,2,3,5,3,2,3,3,3,3]
+"photo": "/images/antonia.png",
+"scores": [2,2,3,5,3,2,3,3,3,3],
+"bio": "Antonia is quiet and loves her cat. Help her break out of her shyness"
 },
 {
 "name": "Rui",
-"photo": "",
-"scores": [4,3,3,4,3,4,3,5,2,3]
+"photo": "/images/rui.jpg",
+"scores": [4,3,3,4,3,4,3,5,2,3],
+"bio": "Rui loves to watch TV. If you need a relaxing night at home, watch 'Friends' and chill with Rui."
 },
 {
 "name": "Stan",
-"photo": "",
-"scores": [4,1,1,3,2,2,4,2,2,1]
+"photo": "images/stan.jpg",
+"scores": [4,1,1,3,2,2,4,2,2,1],
+"bio": "Stan likes to run, and loves coffee! A lot of coffee! Different types of coffee! Try something new with Stan"
 },
 {
 "name": "Susan",
-"photo": "",
-"scores": [1,5,3,3,3,1,1,3,3,3]
+"photo": "images/susan.jpg",
+"scores": [1,5,3,3,3,1,1,3,3,3],
+"bio": "Susan Whitfield likes to read and would love to break out of her shell. She's not into dancing, but she would be fun to watch paint dry with"
 },
 {
 "name": "Mrs Barone",
-"photo": "",
-"scores": [4,1,1,2,2,5,1,4,1,3]
+"photo": "images/barone.png",
+"scores": [4,1,1,2,2,5,1,4,1,3],
+"bio": "Mrs. Barone doesn't get out much or do a lot of physical activity, but if you want to sit and just smoke, she's your perfect match."
 },
 {
 "name": "Manny",
-"photo": "",
-"scores": [4,2,2,4,1,2,1,4,3,3]
+"photo": "images/manny.jpg",
+"scores": [4,2,2,4,1,2,1,4,3,3],
+"bio" : "Manny is hilarious, all his friends say so. He's such a jokester, right guys?"
 },
 {
 "name": "Stuart & Mom",
-"photo": "",
-"scores": [3,3,4,4,5,1,3,3,3,3]
+"photo": "images/stuart.png",
+"scores": [3,3,4,4,5,1,3,3,3,3],
+"bio" : "Stuart's mom comes with some strings attached. Doreen is looking for a father figure for her son Stuart. You'll often need to be there to 'look at what Stuart can do'."
 },
 {
 "name": "Gena",
-"photo": "",
-"scores": [5,4,3,3,3,2,3,3,4,3]
+"photo": "images/gena.jpg",
+"scores": [5,4,3,3,3,2,3,3,4,3],
+"bio" : "Gena (MURDER) loves music and long walks on the beach (MURDER). A romantic dinner is her favorite (MURDER) date night activity (MURDER)."
 }];
 
 
